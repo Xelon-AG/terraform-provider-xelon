8bdd7f5db26b8e616a661cbddaadba02fd0da54b
pavel <36902936+pavel-github@users.noreply.github.com>
2022-12-27 12:03:36 +0100

    chore(resource/xelon_ssh_key): enable acceptance tests

--- internal/xelon/resource_xelon_ssh_key_test.go
+++ internal/xelon/resource_xelon_ssh_key_test.go
@@ -1,36 +1,69 @@
 package xelon
 
-// import (
-// 	"fmt"
-// 	"testing"
-//
-// 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
-// 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-// )
-//
-// func TestAccResourceXelonSSHKey_basic(t *testing.T) {
-// 	sshKeyName := fmt.Sprintf("%s-%s", accTestPrefix, acctest.RandString(10))
-// 	sshKeyPublic := "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAA/yupp+bxv9EKJmg5LNwu1foNjby/Nl++Nx2XTmi80BRRa4daLNQYJ7oQ=="
-// 	config := fmt.Sprintf(testAccResourceXelonSSHKeyConfig, sshKeyName, sshKeyPublic)
-//
-// 	resource.Test(t, resource.TestCase{
-// 		PreCheck:          func() { testAccPreCheck(t) },
-// 		ProviderFactories: testAccProviderFactories,
-// 		Steps: []resource.TestStep{
-// 			{
-// 				Config: config,
-// 				Check: resource.ComposeTestCheckFunc(
-// 					resource.TestCheckResourceAttrSet("xelon_ssh_key.default", "id"),
-// 					resource.TestCheckResourceAttr("xelon_ssh_key.default", "name", sshKeyName),
-// 				),
-// 			},
-// 		},
-// 	})
-// }
-//
-// const testAccResourceXelonSSHKeyConfig = `
-// resource "xelon_ssh_key" "default" {
-//   name = "%s"
-//   public_key = "%s"
-// }
-// `
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/Xelon-AG/xelon-sdk-go/xelon"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+func TestAccResourceXelonSSHKey_basic(t *testing.T) {
+	sshKeyName := fmt.Sprintf("%s-%s", accTestPrefix, acctest.RandString(10))
+	sshKeyPublic := "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAA/yupp+bxv9EKJmg5LNwu1foNjby/Nl++Nx2XTmi80BRRa4daLNQYJ7oQ=="
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: testAccProviderFactories,
+		CheckDestroy:      testAccCheckSSHKeyDestroy,
+
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceXelonSSHKeyConfig(sshKeyName, sshKeyPublic),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("xelon_ssh_key.test", "id"),
+					resource.TestCheckResourceAttrSet("xelon_ssh_key.test", "fingerprint"),
+					resource.TestCheckResourceAttr("xelon_ssh_key.test", "name", sshKeyName),
+					resource.TestCheckResourceAttr("xelon_ssh_key.test", "public_key", sshKeyPublic),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckSSHKeyDestroy(s *terraform.State) error {
+	client := testAccProvider.Meta().(*xelon.Client)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "xelon_ssh_key" {
+			continue
+		}
+
+		sshKeys, _, err := client.SSHKeys.List(context.Background())
+		if err != nil {
+			return err
+		}
+
+		for _, sshKey := range sshKeys {
+			sshKeyID := strconv.Itoa(sshKey.ID)
+			if sshKeyID == rs.Primary.ID {
+				return fmt.Errorf("SSH Key (%s) still exists", rs.Primary.ID)
+			}
+		}
+	}
+
+	return nil
+}
+
+func testAccResourceXelonSSHKeyConfig(name, publicKey string) string {
+	return fmt.Sprintf(`
+resource "xelon_ssh_key" "test" {
+  name = "%s"
+  public_key = "%s"
+}
+`, name, publicKey)
+}
