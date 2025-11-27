217426e557e3f58ace95b5375d0fd98584c310a2
pavel <36902936+pavel-github@users.noreply.github.com>
2025-11-27 01:55:09 +0100

    docs(datasource/xelon_load_balancer): update documentation

---
+++ examples/data-sources/xelon_load_balancer/data-source_with_id_and_name.tf
@@ -0,0 +1,4 @@
+data "xelon_load_balancer" "backend_ui" {
+  id   = "3f831b0bf4"
+  name = "backend UI load balancer"
+}
---
+++ examples/data-sources/xelon_load_balancer/data-source_with_name.tf
@@ -0,0 +1,3 @@
+data "xelon_load_balancer" "backend_ui" {
+  name = "backend UI load balancer"
+}
